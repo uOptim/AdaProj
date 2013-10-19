ab5f8abdf0000af02e72d874f20900f68df7a562
uOptim <uOptim@fakeemail.com>
2013-10-19 16:48:19 +0200

    some line breaks for readability

--- path.adb
+++ path.adb
@@ -5,6 +5,7 @@ package body Path is
 	use Adagraph;
 	package GEF is new Ada.Numerics.Generic_Elementary_Functions(Float);
 
+
 	function Value(From: Points) return Object is
 		Obj: Object(Size => From'Length);
 	begin
@@ -12,8 +13,10 @@ package body Path is
 		return Obj;
 	end Value;
 
+
 	function Segment_Count(Path: in Object) return Natural is
 		(if Path.Values'Length > 0 then Path.Values'Length - 1 else 0);
+
 
 	function Segment_Length(Path: in Object; Segment: in Positive)
 		return Float is
@@ -26,12 +29,14 @@ package body Path is
 		);
 	end;
 
+
 	function "&"(Left: in Object; Right: in Object) return Object is
 		Obj: Object(Left.Values'Length + Right.Values'Length);
 	begin
 		Obj.Values := Left.Values & Right.Values;
 		return Obj;
 	end;
+
 
 	function "&"(Left: in Object; Right: in Point) return Object is
 		Obj: Object(Left.Values'Length+1);
@@ -40,6 +45,7 @@ package body Path is
 		return Obj;
 	end;
 
+
 	function "&"(Left: in Point ; Right: in Object) return Object is
 		Obj: Object(Right.Values'Length+1);
 	begin
@@ -47,12 +53,14 @@ package body Path is
 		return Obj;
 	end;
 
+
 	procedure Add(Path: in out Object; P: in Point) is
 	begin
 		-- TODO: very ineffective!
 		-- double the array size each realloc instead when you got time
 		Path := Path & P;
 	end;
+
 
 	procedure Draw(Path: in Object; Color: in Color_Type := Light_Green) is
 		P_Prev: Point;
