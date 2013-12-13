9eb35589747106f08867904e243c02128515f5e6
uOptim <uOptim@fakeemail.com>
2013-12-14 00:22:08 +0100

    agency mailbox is now a parameter

--- src/agency.ads
+++ src/agency.ads
@@ -3,7 +3,8 @@ with Robot;
 with Mailbox;
 
 generic
-	with package Work_Site is new Site (<>);
+	with package Work_Site   is new Site   (<>);
+	with package Bot_Mailbox is new Mailbox(Message_Type => Work_Site.Bot_ID);
 
 package Agency is
 
@@ -11,13 +12,14 @@ package Agency is
 	procedure Killall;
 
 private
-	package Bot_Mailbox is new Mailbox(Message_Type => Work_Site.Bot_ID);
 	package Robot_Type  is new Robot(
 		Work_Site   => Work_Site,
 		Mailbox_Pkg => Bot_Mailbox
 	);
 
-	Done_Msg_Box: aliased Bot_Mailbox.Object(Size => Work_Site.Bot_ID'Last);
+	Done_Msg_Box: aliased Bot_Mailbox.Object(
+		Size => Work_Site.Bot_ID'Last
+	);
 
 	type Robot_Array is array(Work_Site.Bot_ID'Range)
 		of Robot_Type.Object(MBox => Done_Msg_Box'Access);
--- src/main.adb
+++ src/main.adb
@@ -1,14 +1,19 @@
 with Site;
-with Site.Places_Path;
 with Agency;
+with MailBox;
+with Site.Places_Path;
 
 with Ada.Text_IO;
 
 procedure Main is
 	package IO renames Ada.Text_IO;
 
-	package HexSite   is new Site(NPlaces => 6, NRobots => 4);
-	package HexAgency is new Agency(Work_Site => HexSite);
+	package HexSite     is new Site(NPlaces => 6, NRobots => 2);
+	package Bot_Mailbox is new MailBox(Message_Type => HexSite.Bot_ID);
+	package HexAgency   is new Agency(
+		Work_Site   => HexSite,
+		Bot_Mailbox => Bot_Mailbox
+	);
 
 begin
 	HexSite.Traffic.Start;
